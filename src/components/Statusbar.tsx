eeb3098f7c6efa3c134c02eabfea23e301422a80
Ananya Jain <ananyajain@Amits-MacBook-Air.local>
2025-07-04 10:32:31 +0530

    Logout added,Home page changed

--- src/components/Statusbar.tsx
+++ src/components/Statusbar.tsx
@@ -5,14 +5,14 @@ type StatusBarProps = {
 
 const StatusBar = ({ totalGenerated, totalSolved }: StatusBarProps) => {
   return (
-    <div className="w-full bg-[#0F0F1C] text-white px-4 py-2 flex flex-col sm:flex-row justify-between items-center rounded-lg shadow-md border border-white/10">
-      <div className="flex items-center gap-2">
-        <span className="text-lg font-medium text-white/80">Total Questions Generated:</span>
-        <span className="text-lg font-semibold text-[#A78BFA]">{totalGenerated}</span>
+    <div className="w-full bg-[#0F0F1C] text-white px-4 py-2 flex flex-col gap-2 sm:flex-row justify-between items-center rounded-lg shadow-md border border-white/10">
+      <div className="flex items-center gap-1">
+        <span className="text-lmd font-medium text-white/80">Total Questions Generated: </span>
+        <span className="text-md font-semibold text-[#A78BFA]">{totalGenerated}</span>
       </div>
-      <div className="flex items-center gap-2 mt-2 sm:mt-0">
-        <span className="text-lg font-medium text-white/80">Total Questions Solved:</span>
-        <span className="text-lg font-semibold text-[#34D399]">{totalSolved}</span>
+      <div className="flex items-center gap-1 mt-2 sm:mt-0">
+        <span className="text-md font-medium text-white/80">Total Questions Solved: </span>
+        <span className="text-md font-semibold text-[#34D399]">{totalSolved}</span>
       </div>
     </div>
   );
